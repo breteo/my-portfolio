ba41c4e58c1b07d46062294eacc8fe0cbcd0f27e
breteo <44599374+breteo@users.noreply.github.com>
2024-04-27 23:02:56 -0700

    added basic pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 import "./App.css";
+import About from "../src/Components/About/About";
+import Nav from "./Components/Nav/Nav";
+import Projects from "./Components/Projects/Projects";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <>
+      <header>
+        <Nav />
+      </header>
+      <main>
+        <About />
+        <Projects />
+      </main>
+      <Footer />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/*.{js,ts,jsx,tsx}",
+    "./src/Components/About/*.{js,ts,jsx,tsx}",
+    "./src/Components/Nav/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
+    colors: {
+      'gray': "#b4b4b4",
+    },
     extend: {},
   },
   plugins: [],
-  prefix: 'tw-',
 };
