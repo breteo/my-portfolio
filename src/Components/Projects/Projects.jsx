cd74976059e1ac2b57d1ffe152e6ffeec3894d04
breteo <44599374+breteo@users.noreply.github.com>
2024-05-31 01:13:07 -0700

    renamed image file

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,7 +3,7 @@ import projectData from "./project_details.json";
 import ProjectCard from "../ProjectCard/ProjectCard";
 import Slideshow from "../Slideshow/Slideshow";
 import robotPic from "../../assets/ProjectImages/robot.jpg";
-import pprPic from "../../assets/ProjectImages/PayPerRead.png";
+import pprPic from "../../assets/ProjectImages/ppr.png";
 import slugPic from "../../assets/ProjectImages/slugstore.jpg";
 import dashboardPic from "../../assets/ProjectImages/dashboard.png";
 
